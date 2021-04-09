79d7270bee2dfaa30773d32112f93b41d2fa85de
Dawid <dannn.dawid@gmail.com>
2021-04-09 23:40:43 +0200

    Created User entity and Comment entity, downgrade Doctrin's version

---
+++ api/src/Entity/Comment.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_movie;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $id_user;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $comment;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdMovie(): ?int
+    {
+        return $this->id_movie;
+    }
+
+    public function setIdMovie(int $id_movie): self
+    {
+        $this->id_movie = $id_movie;
+
+        return $this;
+    }
+
+    public function getIdUser(): ?User
+    {
+        return $this->id_user;
+    }
+
+    public function setIdUser(?User $id_user): self
+    {
+        $this->id_user = $id_user;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
